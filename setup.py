b33aa1bf02e3a8c5e1563d173513a9088fab4be1
isaac-barnhart <isaac.barnhart@corteva.com>
2024-10-28 14:53:26 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nc_to_gpkg",
-    version="0.0.2",
+    version="0.0.7",
     description="A package for processing NetCDF files and merging with shapefiles",
     author="Isaac Barnhart",
     author_email="isaac.barnhart@corteva.com",
